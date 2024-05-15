b1c96626d2e1c078a010218787703ced66fcf816
Hancheol Cho <chcbaram@gmail.com>
2024-05-15 11:58:58 +0900

    45K 엔터키 키맵 변경

--- src/ap/modules/qmk/keyboards/baram/45k/keymap.c
+++ src/ap/modules/qmk/keyboards/baram/45k/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
     [0] = {
         {KC_ESC,        KC_Q,    KC_W,    KC_E,         KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC  },
-        {KC_TAB,        KC_A,    KC_S,    KC_D,         KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_PENT  },
+        {KC_TAB,        KC_A,    KC_S,    KC_D,         KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT   },
         {LM(2,MOD_LSFT),KC_Z,    KC_X,    KC_C,         KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   MO(3)    },
         {KC_LCTL,       KC_LGUI, KC_LALT, LT(1,KC_SPC), _______, _______, KC_SPC,  MO(5),   KC_LEFT, _______, KC_DOWN, KC_RGHT  }
     }         
