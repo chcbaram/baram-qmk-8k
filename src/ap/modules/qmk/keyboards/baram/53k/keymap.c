9bbe331ee5cfc29edacf0a933266474262ebf8b9
Hancheol Cho <chcbaram@gmail.com>
2024-06-12 14:58:11 +0900

    코드 정리

--- src/ap/modules/qmk/keyboards/baram/53k/keymap.c
+++ src/ap/modules/qmk/keyboards/baram/53k/keymap.c
@@ -14,9 +14,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______ }, // 2
         {KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  _______, _______, KC_RALT, _______ }, // 3
 
-        {KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL }, // 0 
-        {KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP}, // 2
-        {KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN}, // 3
-        {_______, _______, _______, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT}  // 4
+        {KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL  }, // 0 
+        {KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP }, // 2
+        {KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN }, // 3
+        {_______, _______, _______, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT }  // 4
     }         
 };
