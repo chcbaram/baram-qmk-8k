194466feb24bb64c0c771fab2ebce04568a5c923
Hancheol Cho <chcbaram@gmail.com>
2024-06-14 22:34:26 +0900

    BARAM-8K-60 추가

---
+++ src/ap/modules/qmk/keyboards/baram/60/config.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+#define KBD_NAME                    "BARAM-8K-60"
+
+#define USB_VID                     0x0483
+#define USB_PID                     0x5204
+
+#define EEPROM_CHIP_ZD24C128 
+#define EECONFIG_USER_DATA_SIZE     1024
+#define TOTAL_EEPROM_BYTE_COUNT     8192
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT  8
+
+#define MATRIX_ROWS                 5
+#define MATRIX_COLS                 16
+
+#define DEBOUNCE                    10
+
+#define DEBUG_MATRIX_SCAN_RATE
+#define GRAVE_ESC_ENABLE
--- src/hw/hw_def.h
+++ src/hw/hw_def.h
@@ -7,7 +7,7 @@
 #include QMK_KEYMAP_CONFIG_H
 
 
-#define _DEF_FIRMWATRE_VERSION    "V240613R1"
+#define _DEF_FIRMWATRE_VERSION    "V240614R1"
 #define _DEF_BOARD_NAME           "BARAM-QMK-8K-FW"
 
 
