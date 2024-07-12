0f3c3fb7393327339e87d7ccb55950db454a44d7
Hancheol Cho <chcbaram@gmail.com>
2024-07-12 22:56:28 +0900

    suspend 기능 추가

--- src/ap/modules/qmk/keyboards/baram/68K/config.cmake
+++ src/ap/modules/qmk/keyboards/baram/68K/config.cmake
@@ -6,11 +6,6 @@ cmake_minimum_required(VERSION 3.13)
 #
 set(RGBLIGHT_ENABLE true)
 
-add_compile_definitions(RGBLED_NUM=68)
-add_compile_definitions(RGBLIGHT_EFFECT_RGB_TEST)
-add_compile_definitions(RGBLIGHT_EFFECT_BREATHING)
-add_compile_definitions(RGBLIGHT_EFFECT_RAINBOW_MOOD)
-add_compile_definitions(RGBLIGHT_EFFECT_SNAKE)
-add_compile_definitions(RGBLIGHT_EFFECT_STATIC_GRADIENT)
+
 
 
