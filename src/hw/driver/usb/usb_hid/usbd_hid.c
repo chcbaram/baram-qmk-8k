e1483dc1a252da964d86b5aadc469dc4b94ed5ca
Hancheol Cho <chcbaram@gmail.com>
2024-08-31 12:18:35 +0900

    USB 키 입력 범위 리포트 수정

--- src/hw/driver/usb/usb_hid/usbd_hid.c
+++ src/hw/driver/usb/usb_hid/usbd_hid.c
@@ -403,10 +403,10 @@ __ALIGN_BEGIN static uint8_t HID_KEYBOARD_ReportDesc[HID_KEYBOARD_REPORT_DESC_SI
   0x95, HW_KEYS_PRESS_MAX,            //   REPORT_COUNT (6)
   0x75, 0x08,                         //   REPORT_SIZE (8)
   0x15, 0x00,                         //   LOGICAL_MINIMUM (0)
-  0x25, 0x65,                         //   LOGICAL_MAXIMUM (101)
+  0x25, 0xFF,                         //   LOGICAL_MAXIMUM (255)
   0x05, 0x07,                         //   USAGE_PAGE (Keyboard)
   0x19, 0x00,                         //   USAGE_MINIMUM (Reserved (no event indicated))
-  0x29, 0x65,                         //   USAGE_MAXIMUM (Keyboard Application)
+  0x29, 0xFF,                         //   USAGE_MAXIMUM (Keyboard Application)
   0x81, 0x00,                         //   INPUT (Data,Ary,Abs)
   0xc0                                // END_COLLECTION
 };
