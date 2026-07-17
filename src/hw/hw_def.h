3030518331da8cae0c755cb1f6b5f58f2b3e7292
Hancheol Cho <chcbaram@gmail.com>
2026-07-17 17:44:43 +0900

    펌웨어 버전 V260717R1

--- src/hw/hw_def.h
+++ src/hw/hw_def.h
@@ -7,7 +7,7 @@
 #include QMK_KEYMAP_CONFIG_H
 
 
-#define _DEF_FIRMWATRE_VERSION    "V260715R1"
+#define _DEF_FIRMWATRE_VERSION    "V260717R1"
 #define _DEF_BOARD_NAME           "BARAM-QMK-8K-FW"
 
 
