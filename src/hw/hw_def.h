e4aa0b8c80a4c844b1f04faa3bcfa7bf32b88602
Hancheol Cho <chcbaram@gmail.com>
2026-09-11 22:19:22 +0900

    Firmware version V260911R1

--- src/hw/hw_def.h
+++ src/hw/hw_def.h
@@ -7,7 +7,7 @@
 #include QMK_KEYMAP_CONFIG_H
 
 
-#define _DEF_FIRMWATRE_VERSION    "V260904R1"
+#define _DEF_FIRMWATRE_VERSION    "V260911R1"
 #define _DEF_BOARD_NAME           "BARAM-QMK-8K-FW"
 
 
